e9a765685a30e23bb4a51802c38a2c0a8dd95578
ngittlen <nathaniel.gittlen@gmail.com>
2019-08-21 15:22:15 -0400

    example cards

--- service/hello-world.js
+++ service/hello-world.js
@@ -23,7 +23,46 @@ module.exports = function(req, res, next) {
   then((response) => {
     console.log('response' + response.data.result)
     let aCard = new Card( "Hello World", response.data.result , "label", "url", "info");
-    let responseObject =  { payload: { cards: [ aCard ]} };
+    //let responseObject =  { payload: { cards: [ aCard ]} };
+    let responseObject = JSON.parse("{\n"
+        + "  \"cards\": [\n"
+        + "    {\n"
+        + "      \"summary\": \"Example Card\",\n"
+        + "      \"indicator\": \"info\",\n"
+        + "      \"detail\": \"This is an example card.\",\n"
+        + "      \"source\": {\n"
+        + "        \"label\": \"Static CDS Service Example\",\n"
+        + "        \"url\": \"https://example.com\",\n"
+        + "        \"icon\": \"https://example.com/img/icon-100px.png\"\n"
+        + "      },\n"
+        + "      \"links\": [\n"
+        + "        {\n"
+        + "          \"label\": \"Google\",\n"
+        + "          \"url\": \"https://google.com\",\n"
+        + "          \"type\": \"absolute\"\n"
+        + "        },\n"
+        + "        {\n"
+        + "          \"label\": \"Github\",\n"
+        + "          \"url\": \"https://github.com\",\n"
+        + "          \"type\": \"absolute\"\n"
+        + "        },\n"
+        + "        {\n"
+        + "          \"label\": \"SMART Example App\",\n"
+        + "          \"url\": \"https://smart.example.com/launch\",\n"
+        + "          \"type\": \"smart\",\n"
+        + "          \"appContext\": \"{\\\"session\\\":3456356,\\\"settings\\\":{\\\"module\\\":4235}}\"\n"
+        + "        }\n"
+        + "      ]\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"summary\": \"Another card\",\n"
+        + "      \"indicator\": \"warning\",\n"
+        + "      \"source\": {\n"
+        + "        \"label\": \"Static CDS Service Example\"\n"
+        + "      }\n"
+        + "    }\n"
+        + "  ]\n"
+        + "}");
     res.send( responseObject );
   });
 
