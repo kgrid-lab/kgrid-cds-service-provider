1782bd551c5fa57a6995fcb6bbe8c7b97037f870
ngittlen <nathaniel.gittlen@gmail.com>
2019-08-21 15:51:44 -0400

    cleaned up links array

--- service/hello-world.js
+++ service/hello-world.js
@@ -17,38 +17,20 @@ module.exports = function(req, res, next) {
     headers: { Accept: 'application/json' }
   };
   const url = 'http://kgrid-activator.herokuapp.com/hello/world/v0.2.0/welcome';
-  console.log("BODY IS: " + JSON.stringify(req.body, null, 4));
   const data = {"name": req.body.prefetch.patient.name[0].given[0] };
 
   axios.post(url, data , axiosConfig ).
   then((response) => {
     console.log('response' + response.data.result)
 
-    let links = JSON.parse("[\n"
-        + "        {\n"
-        + "          \"label\": \"Google\",\n"
-        + "          \"url\": \"https://google.com\",\n"
-        + "          \"type\": \"absolute\"\n"
-        + "        },\n"
-        + "        {\n"
-        + "          \"label\": \"Github\",\n"
-        + "          \"url\": \"https://github.com\",\n"
-        + "          \"type\": \"absolute\"\n"
-        + "        },\n"
-        + "        {\n"
-        + "          \"label\": \"SMART Example App\",\n"
-        + "          \"url\": \"https://smart.example.com/launch\",\n"
-        + "          \"type\": \"smart\",\n"
-        + "          \"appContext\": \"{\\\"session\\\":3456356,\\\"settings\\\":{\\\"module\\\":4235}}\"\n"
-        + "        }\n"
-        + "      ]");
+    links = [ { label: "google", url: "https://www.google.com", type: "absolute"},
+      { label: "github", url: "https://www.github.com", type: "absolute"}];
     let aCard = new Card( "Hello World", response.data.result , "label", "url", "info", JSON.parse(JSON.stringify(links)));
-    links.push({label: "new link", url: "http://yahoo.com", type: "absolute"});
+    links.push({label: "yahoo", url: "http://yahoo.com", type: "absolute"});
     let card2 = new Card("Hello links", response.data.result, "label", "http://umich.edu", "warning", links);
     let responseObject =  { cards: [ aCard, card2 ]};
     res.send( responseObject );
   });
-
 
 };
 
